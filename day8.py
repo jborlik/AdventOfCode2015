a4f685921b94b8f74ebc99d1b59fe713fb7af32a
Jeffrey Borlik <jborlik@gmail.com>
2015-12-11 22:46:03 -0800

    Adding the first half of Day 8

---
+++ day8.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+
+# The only escape sequences used are:
+#   \\ (which represents a single backslash), 
+#   \" (which represents a lone double-quote character), 
+#   and \x plus two hexadecimal characters (which represents a single character with that ASCII code)
+re_test1 = re.compile(r'\\\\')
+re_test2 = re.compile(r'\\"')
+re_test3 = re.compile(r'\\x[0-9a-f]{2,2}')
+
+
+totalcodechars = 0
+totalmemchars = 0
+
+with open('day8.dat') as datafile:
+    
+    for thisstring in datafile:
+        thisstring = thisstring.rstrip()
+        codechars = len(thisstring)
+        totalcodechars += codechars
+        
+        # remove start/end quotes
+        thisstring = thisstring[1:-1]
+        # resolve \\  to 0
+        thisstring = re_test1.sub('0', thisstring)
+        # resolve \"  to 0
+        thisstring = re_test2.sub('0', thisstring)
+        # resolve \xHH to 0
+        thisstring = re_test3.sub('0', thisstring)
+        
+        memchars = len(thisstring)
+        totalmemchars += memchars
+        
+
+print("Code chars = {0}, mem chars = {1}, diff = {2}".format(totalcodechars,totalmemchars,totalcodechars-totalmemchars))
+    
